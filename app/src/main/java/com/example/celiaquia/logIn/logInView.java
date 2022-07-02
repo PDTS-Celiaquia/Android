939ef98ad74d951d50aa08ead75a4adacd7bc9a9
Nacho-C <nachomercado@live.com>
2022-07-02 19:05:53 -0300

    validaciones en login y registro

--- app/src/main/java/com/example/celiaquia/logIn/logInView.java
+++ app/src/main/java/com/example/celiaquia/logIn/logInView.java
@@ -2,30 +2,48 @@ package com.example.celiaquia.logIn;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.ColorInt;
+
 import com.example.celiaquia.R;
 import com.example.celiaquia.registro.registroView;
 
+import java.util.StringTokenizer;
+
 public class logInView extends Activity {
+
+    EditText email, password;
+    boolean emailValidado = false, passValidada = false;
+
+    private void apagarBoton(Button boton) {
+        boton.setClickable(false);
+        boton.setBackgroundColor(getResources().getColor(R.color.grisclarito));
+    }
+
+    private void prenderBoton(Button boton, View.OnClickListener listener) {
+        boton.setOnClickListener(listener);
+        boton.setBackgroundColor(getResources().getColor(R.color.naranja));
+    }
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
 
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
         Button ingresar = findViewById(R.id.ingresar);
-        Button registrar = findViewById(R.id.registrarse);
-        ingresar.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener clickListenerIngresar = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText email, password;
-                email = findViewById(R.id.email);
-                password = findViewById(R.id.password);
-
                 logIn login= new logIn();
                 login.logear(email,password);
 
@@ -34,16 +52,71 @@ public class logInView extends Activity {
                     case(200):
                         toast = Toast.makeText(getApplicationContext(), "Ingreso con exito", Toast.LENGTH_SHORT);
                         break;
-                    case(401):
-                        toast = Toast.makeText(getApplicationContext(), "Email y/o contraseña incorrectos", Toast.LENGTH_SHORT);
-                        break;
                     default:
                         toast = Toast.makeText(getApplicationContext(), "Error inesperado - "+login.getCodigo()+" - "+login.getMensaje(), Toast.LENGTH_SHORT);
                 }
                 toast.show();
             }
+        };
+
+        //Validación de email
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null && android.util.Patterns.EMAIL_ADDRESS.matcher(s).matches())
+                {
+                    //email valido, activar boton ingresar y poner campo en verde
+                    emailValidado = true;
+                    if (passValidada)
+                        prenderBoton(ingresar,clickListenerIngresar);
+                } else {
+                    //email invalido, apagar boton ingresar y poner campo en rojo
+                    emailValidado = false;
+                    apagarBoton(ingresar);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
         });
 
+        //Validación de contraseña
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null && s.length() > 0) //ACÁ AGREGAR CONDICIONES PARA CONTRASEÑA (mayus, minus, etc.)
+                {
+                    //pass valida, activar boton ingresar y poner campo en verde
+                    passValidada = true;
+                    if (emailValidado) {
+                        prenderBoton(ingresar,clickListenerIngresar);
+
+                    }
+                } else {
+                    //pass invalida, apagar boton ingresar y poner campo en rojo
+                    passValidada = false;
+                    apagarBoton(ingresar);
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        Button registrar = findViewById(R.id.registrarse);
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/celiaquia/registro/registroView.java
+++ app/src/main/java/com/example/celiaquia/registro/registroView.java
@@ -2,6 +2,8 @@ package com.example.celiaquia.registro;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,20 +12,32 @@ import android.widget.Toast;
 import com.example.celiaquia.R;
 
 public class registroView extends Activity {
+    EditText email, password,nombre,apellido;
+    boolean emailValidado = false, passValidada = false, nombreValidado = false, apellidoValidado = false;
+
+    private void apagarBoton(Button boton) {
+        boton.setClickable(false);
+        boton.setBackgroundColor(getResources().getColor(R.color.grisclarito));
+    }
+
+    private void prenderBoton(Button boton, View.OnClickListener listener) {
+        boton.setOnClickListener(listener);
+        boton.setBackgroundColor(getResources().getColor(R.color.naranja));
+    }
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
 
+        nombre = findViewById(R.id.nombre);
+        apellido = findViewById(R.id.apellido);
+        email = findViewById(R.id.emailr);
+        password = findViewById(R.id.passwordr);
+
         Button registrar = findViewById(R.id.registrarse);
-        registrar.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener clickListenerRegistrar = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText email, password,nombre,apellido;
-                nombre = findViewById(R.id.nombre);
-                apellido = findViewById(R.id.apellido);
-                email = findViewById(R.id.emailr);
-                password = findViewById(R.id.passwordr);
-
                 registro reg= new registro();
                 reg.registrar(nombre,apellido,email,password);
 
@@ -32,13 +46,121 @@ public class registroView extends Activity {
                     case(200):
                         toast = Toast.makeText(getApplicationContext(), "Ingreso con éxito", Toast.LENGTH_SHORT);
                         break;
-                    case(401):
-                        toast = Toast.makeText(getApplicationContext(), reg.getMensaje(), Toast.LENGTH_SHORT);
-                        break;
                     default:
                         toast = Toast.makeText(getApplicationContext(), "Error inesperado - "+reg.getCodigo()+" - "+reg.getMensaje(), Toast.LENGTH_SHORT);
                 }
                 toast.show();
+            }
+        };
+
+        //Validación de email
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null && android.util.Patterns.EMAIL_ADDRESS.matcher(s).matches())
+                {
+                    //email valido, activar boton ingresar y poner campo en verde
+                    emailValidado = true;
+                    if (passValidada && nombreValidado && apellidoValidado)
+                        prenderBoton(registrar,clickListenerRegistrar);
+                } else {
+                    //email invalido, apagar boton ingresar y poner campo en rojo
+                    emailValidado = false;
+                    apagarBoton(registrar);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //Validación de contraseña
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null && s.length() > 0) //ACÁ AGREGAR CONDICIONES PARA CONTRASEÑA (mayus, minus, etc.)
+                {
+                    //pass valida, activar boton ingresar y poner campo en verde
+                    passValidada = true;
+                    if (emailValidado && nombreValidado && apellidoValidado) {
+                        prenderBoton(registrar,clickListenerRegistrar);
+                    }
+                } else {
+                    //pass invalida, apagar boton ingresar y poner campo en rojo
+                    passValidada = false;
+                    apagarBoton(registrar);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //Validación de nombre
+        nombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null && s.length() > 0) //ACÁ AGREGAR CONDICIONES PARA NOMBRE
+                {
+                    //nombre valido, activar boton ingresar y poner campo en verde
+                    nombreValidado = true;
+                    if (emailValidado && passValidada && apellidoValidado) {
+                        prenderBoton(registrar,clickListenerRegistrar);
+                    }
+                } else {
+                    //nombre invalido, apagar boton ingresar y poner campo en rojo
+                    nombreValidado = false;
+                    apagarBoton(registrar);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //Validación de apellido
+        apellido.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null && s.length() > 0) //ACÁ AGREGAR CONDICIONES PARA APELLIDO (mayus, minus, etc.)
+                {
+                    //apellido valido, activar boton ingresar y poner campo en verde
+                    apellidoValidado = true;
+                    if (emailValidado && nombreValidado && passValidada) {
+                        prenderBoton(registrar,clickListenerRegistrar);
+                    }
+                } else {
+                    //apellido invalido, apagar boton ingresar y poner campo en rojo
+                    apellidoValidado = false;
+                    apagarBoton(registrar);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
